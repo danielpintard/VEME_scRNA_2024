6bc144eb6d59380722a125e76aac165fe8fa23a2
danielpintard <100798259+danielpintard@users.noreply.github.com>
2024-08-07 23:13:01 -0400

    updated

--- download.sh
+++ download.sh
@@ -1,33 +1,23 @@
 #!/bin/bash
 
 # Arrays of file IDs and file names
-file_ids=("1iN0dpYonMwxzp7FhnphM8t0Rc9AJj5Mw" "1eV_fVVBfOydwyojrFlXREACMxBPafLtz" "1Rx8kFwbAb4GDkrkQruyDMUuVlmlPISmf" "1ZrlvM8ZV_WQFsSGUjxDwDk31GodbzADV" "1E1MslSlpY_dU--AMGf6gIBLM9Zqv7HSh" "1tAeBrp_wlf2R-iJ7UnQ6Uh4bOxQfH3WT")
+file_ids=("1yuzFqBh0e_HEimeYYTYG-O8aFGGZZjcm" "14RL8Q55oe2RowVIfaAU2QycC4mfRYRPU" "1GmzlAQ7mRLqQTeMCHpb2PAz67_kydpT0" "13nhDDgyQhGGRRmcTBY1yV-OVbPvSljos" "1hPWLMSse7hWD_gfArsU5Wtwy6ugKSVee" "1QeUNDlR9BTAa7LUJjUjPgnoVkereXi01")
 file_names=("GSE171524_downsample.h5ad" "GSE171524_clustered.h5ad" "GSE171524_integrated.h5ad" "GSE171524_post_qc.h5ad" "model.pt" "adata.h5ad")
 
 # Folder to save the files
 output_folder="data"
 mkdir -p $output_folder
 
-# Start time
-start_time=$SECONDS
-
-# Download each file and measure time
+# Download each file
 for i in "${!file_ids[@]}"; do
     file_id=${file_ids[$i]}
     file_name=${file_names[$i]}
-    
-    echo "Downloading ${file_name}..."
-    file_start_time=$SECONDS
-    
     gdown "https://drive.google.com/uc?id=${file_id}" -O "${output_folder}/${file_name}"
-    
-    file_elapsed_time=$(($SECONDS - $file_start_time))
-    echo "Downloaded ${file_name} in ${file_elapsed_time} seconds."
 done
 
-# Total elapsed time
-total_elapsed_time=$(($SECONDS - $start_time))
-echo "Total time: ${total_elapsed_time} seconds."
-
+# Move specific files to a new directory
 mkdir -p 'model'
-mv data/adata.h5ad model/; mv data/model.pt model/
+mv data/adata.h5ad model/
+mv data/model.pt model/
+
+
